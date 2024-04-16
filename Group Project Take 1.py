ee2b986fdfe91d26053e0b73529847ca2a4daddf
WaltersJ07 <WaltersJ07@uww.edu>
2024-04-16 17:30:16 -0500

    Take 2

--- Group Project Take 1.py
+++ Group Project Take 1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load the data
 batting_df = pd.read_excel('C:/Users/WaltersJ07/Downloads/Batting.xlsx')
@@ -22,3 +23,13 @@ average_batting_by_age = df.groupby('age')['batting_average'].mean()
 peak_batting_age = average_batting_by_age.idxmax()
 
 print(f'The age with the highest average batting average is {peak_batting_age}.')
+
+# Create a plot
+plt.figure(figsize=(10, 6))
+plt.plot(average_batting_by_age.index, average_batting_by_age.values)
+plt.scatter(peak_batting_age, average_batting_by_age.max(), color='red')  # mark the peak
+plt.xlabel('Age')
+plt.ylabel('Average Batting Average')
+plt.title('Average Batting Average by Age')
+plt.grid(True)
+plt.show()
