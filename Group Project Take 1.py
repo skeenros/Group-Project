ce97d6b4a29700d52dd8215d594645a70a342b17
WaltersJ07 <WaltersJ07@uww.edu>
2024-04-17 10:18:12 -0500

    Take 2 Batting Average

--- Group Project Take 1.py
+++ Group Project Take 1.py
@@ -3,31 +3,32 @@ import matplotlib.pyplot as plt
 
 # Load the data
 batting_df = pd.read_excel('C:/Users/WaltersJ07/Downloads/Batting.xlsx')
-pitching_df = pd.read_excel('C:/Users/WaltersJ07/Downloads/Pitching.xlsx')
 people_df = pd.read_excel('C:/Users/WaltersJ07/Downloads/People.xlsx')
-
-# Calculate the batting average for each player in the batting dataframe
-batting_df['batting_average'] = batting_df['H'] / batting_df['AB']
 
 # Join the data on the playerID
 df = pd.merge(batting_df, people_df, on='playerID')
-df = pd.merge(df, pitching_df, on=['playerID', 'yearID'])
 
 # Calculate the players' ages
 df['age'] = df['yearID'] - df['birthYear']
 
-# Group the data by age and calculate the average batting average in each age group
-average_batting_by_age = df.groupby('age')['batting_average'].mean()
+# Filter out records where 'AB' is 0 and 'age' is not within a reasonable range
+df = df[(df['AB'] != 0) & (df['age'] >= 19) & (df['age'] <= 45)]
+
+# Group the data by age and sum up 'H' and 'AB' for each age
+sums_by_age = df.groupby('age')[['H', 'AB']].sum()
+
+# Calculate the batting average for each age
+sums_by_age['batting_average'] = sums_by_age['H'] / sums_by_age['AB']
 
 # Find the age with the highest average batting average
-peak_batting_age = average_batting_by_age.idxmax()
+peak_batting_age = sums_by_age['batting_average'].idxmax()
 
 print(f'The age with the highest average batting average is {peak_batting_age}.')
 
 # Create a plot
 plt.figure(figsize=(10, 6))
-plt.plot(average_batting_by_age.index, average_batting_by_age.values)
-plt.scatter(peak_batting_age, average_batting_by_age.max(), color='red')  # mark the peak
+plt.plot(sums_by_age.index, sums_by_age['batting_average'])
+plt.scatter(peak_batting_age, sums_by_age['batting_average'].max(), color='red')  # mark the peak
 plt.xlabel('Age')
 plt.ylabel('Average Batting Average')
 plt.title('Average Batting Average by Age')
